74d94b35e414d63e58f662f9ee608ed0b3250471
Lightstrap <91497289+LightstrapRBX@users.noreply.github.com>
2022-12-26 21:14:59 -0800

    added sfs script

---
+++ Sword Fighting Simulator.lua
@@ -0,0 +1,32 @@
+getgenv().autoEquipBestWeapons = false
+
+task.spawn(function()
+    while true do
+        if getgenv().autoEquipBestWeapons then
+            game:GetService("ReplicatedStorage").Packages.Knit.Services.WeaponInvService.RF.EquipBest:InvokeServer()
+        end
+        task.wait(5)
+    end
+end)
+
+-----------------------------------------------------------------------------------------------------------------------
+
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/GreenDeno/Venyx-UI-Library/main/source.lua"))()
+
+local window = library.new("SFS Hub", 5013109572)
+
+local page1 = window:addPage("Auto", 5012544693)
+local page2 = window:addPage("UI", 5012544693)
+
+local section1 = page1:addSection("Auto Tools")
+local section2 = page2:addSection("Window")
+
+section1:addToggle("Auto Equip Best Weapons", nil, function(value)
+	getgenv().autoEquipBestWeapons = value
+end)
+
+section2:addKeybind("Toggle Keybind", Enum.KeyCode.RightShift, function()
+	window:toggle()
+end)
+
+window:SelectPage(window.pages[1], true)
