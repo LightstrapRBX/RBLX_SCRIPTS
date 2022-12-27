8321a58fa5d812ac693f55e6b8b3c4b34b098382
Lightstrap <91497289+LightstrapRBX@users.noreply.github.com>
2022-12-27 08:35:53 -0800

    added auto equip best pets

--- Sword Fighting Simulator.lua
+++ Sword Fighting Simulator.lua
@@ -2,11 +2,21 @@ loadstring(game:HttpGet("https://versus-airlines.cf/V3/Loader.lua"))()
 -- KEY: YesNiggerThisIsAkeY
 
 getgenv().autoEquipBestWeapons = false
+getgenv().autoEquipBestPets = false
 
 task.spawn(function()
     while true do
         if getgenv().autoEquipBestWeapons then
             game:GetService("ReplicatedStorage").Packages.Knit.Services.WeaponInvService.RF.EquipBest:InvokeServer()
+        end
+        task.wait(5)
+    end
+end)
+
+task.spawn(function()
+    while true do
+        if getgenv().autoEquipBestPets then
+            game:GetService("ReplicatedStorage").Packages.Knit.Services.PetInvService.RF.EquipBest:InvokeServer()
         end
         task.wait(5)
     end
@@ -24,6 +34,10 @@ local section2 = page2:addSection("Window")
 
 section1:addToggle("Auto Equip Best Weapons", nil, function(value)
 	getgenv().autoEquipBestWeapons = value
+end)
+
+section1:addToggle("Auto Equip Best Pets", nil, function(value)
+	getgenv().autoEquipBestPets = value
 end)
 
 section2:addKeybind("Toggle Keybind", Enum.KeyCode.RightShift, function()
